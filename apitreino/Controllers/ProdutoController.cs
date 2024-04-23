53917501cfef413e065b7ec6fac9f7ac4437b746
Vinicius Nogueira Duran <Vinicius3005@Zicard.com.br>
2024-04-23 16:30:26 -0300

    first

---
+++ apitreino/Controllers/ProdutoController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace apitreino.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProdutoController : ControllerBase
+    {
+        private static List<Produto> _produtos = new List<Produto>();
+        private static int _contador = 1;
+
+        [HttpGet]
+        public IEnumerable<Produto> Get()
+        {
+            return _produtos;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var produto = _produtos.FirstOrDefault(p => p.Id == id);
+            if (produto == null)
+            {
+                return NotFound();
+            }
+            return Ok(produto);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Produto produto)
+        {
+            produto.Id = _contador++;
+            _produtos.Add(produto);
+            return CreatedAtAction(nameof(Get), new { id = produto.Id }, produto);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Produto produto)
+        {
+            var index = _produtos.FindIndex(p => p.Id == id);
+            if (index < 0)
+            {
+                return NotFound();
+            }
+            _produtos[index] = produto;
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var produto = _produtos.FirstOrDefault(p => p.Id == id);
+            if (produto == null)
+            {
+                return NotFound();
+            }
+            _produtos.Remove(produto);
+            return Ok(produto);
+        }
+    }
+}
---
+++ apitreino/Startup.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+
+namespace apitreino
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            // Adicionar o Swagger
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "NomeDaSuaAPI", Version = "v1" });
+            });
+
+            // Adicionar suporte para CORS
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAnyOrigin", builder =>
+                {
+                    builder.AllowAnyOrigin()
+                           .AllowAnyHeader()
+                           .AllowAnyMethod();
+                });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            // Habilitar CORS
+            app.UseCors("AllowAnyOrigin");
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            // Configuração do Swagger
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "NomeDaSuaAPI v1");
+            });
+        }
+    }
+}
